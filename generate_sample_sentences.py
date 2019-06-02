2f374404936d9dc4430eaed78376c10359abf4bf
Kushal Tirumala <kushaltirumala@Kushals-MacBook-Pro.local>
2019-06-02 15:16:06 -0700

    no sentiment here

--- generate_sample_sentences.py
+++ generate_sample_sentences.py
@@ -65,10 +65,10 @@ for i in range(int(args.num_sentences)):
     c = model.sample_c_prior(1)
     print(c_dim)
 
-    _, c_idx = torch.max(c, dim=7)
+    _, c_idx = torch.max(c, dim=1)
     sample_idxs = model.sample_sentence(z, c, temp=0.1)
 
-    print('\nSentiment: {}'.format(dataset.idx2label(int(c_idx))))
+    # print('\nSentiment: {}'.format(dataset.idx2label(int(c_idx))))
     print('Generated: {}'.format(dataset.idxs2sentence(sample_idxs)))
 
 
