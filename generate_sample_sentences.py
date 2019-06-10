a21ad2c58bc2d5abdf545339ad2d56ffed6e0652
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-06-10 05:49:45 +0000

    committing before removing

--- generate_sample_sentences.py
+++ generate_sample_sentences.py
@@ -24,7 +24,7 @@ parser.add_argument('--gpu', default=False, action='store_true',
                     help='whether to run in the GPU')
 parser.add_argument('--model', default='ctextgen', metavar='',
                     help='choose the model: {`vae`, `ctextgen`}, (default: `ctextgen`)')
-parser.add_argument('--path', default='saved_models/emotions_7_tfidf_wordvec.bin',
+parser.add_argument('--path', default='saved_models/tfidf_7_emotion_gmm_wordvec.bin',
                     metavar='', help='choose the model: from saved_models, (default: `baseline_vae`)')
 parser.add_argument('--num_sentences', default='10')
 
